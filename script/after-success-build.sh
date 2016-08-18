8cbe130d847f8e3a8952b1a5c8e540f9923def25
Jonathan A. Schweder <jonathanschweder@gmail.com>
2016-08-18 15:45:45 -0300

    add script run to trigger 5.6-laravel build

---
+++ script/after-success-build.sh
@@ -0,0 +1,8 @@
+# Trigger downstream images
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token $TRAVIS_TOKEN" \
+  -d "{\"request\":{\"branch\":\"5.6-laravel\"}}" \
+  https://api.travis-ci.org/repo/jaschweder%2Fdocker-image-php/requests
