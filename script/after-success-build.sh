9f87c3ffa6215d38c16e9f6b30f1c66d0470fa36
Jonathan A. Schweder <jonathanschweder@gmail.com>
2016-08-19 14:40:16 -0300

    test matrix

--- script/after-success-build.sh
+++ script/after-success-build.sh
@@ -1,4 +1,7 @@
-# Trigger downstream images
+
+# Trigger dependencie branch
+if [ "$DOWNSTREAM" = "true" ]
+then
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
@@ -6,3 +9,4 @@ curl -s -X POST \
   -H "Authorization: token $TRAVIS_TOKEN" \
   -d "{\"request\":{\"branch\":\"server\"}}" \
   https://api.travis-ci.org/repo/jaschweder%2Fdocker-image-php/requests
+fi
